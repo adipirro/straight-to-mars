1fa9fab37e3940811cb896718ea96b102dda39de
Anthony DiPirro <adipirro@nexient.com>
2016-06-03 21:20:41 -0400

    Character controller stuff and basic folder setup

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	int i;
+	// Use this for initialization
+	void Start () {
+		i = 0;
+		Debug.Log ("Hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (i++);
+		transform.Translate(new Vector3(1,0,0) * 0);
+	}
+}
