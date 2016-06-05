ceacb9156896ed03cfa39efcd25dc57f4f02a0ad
Nathan Thomas <nthomas@systemsinmotion.com>
2016-06-05 10:46:52 -0400

    merged in teleport function

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -18,9 +18,13 @@ public class PlaneController : MonoBehaviour {
 		var MattDamon = Instantiate (Resources.Load ("Prefabs/Character/Matt Damon")) as GameObject;
 		MattDamon.transform.position = new Vector3 (128, 128, 128);
 	}
-	
+
+	int counter = 0;
 	// Update is called once per frame
 	void Update () {
+//		counter++;
+//		if (counter == 100)
+//			teleport (22, 44);
 	}
 
 	public void instantiateQuadPlane(int row, int col) {
@@ -28,5 +32,19 @@ public class PlaneController : MonoBehaviour {
 			quadPlane[row, col] = (GameObject) Instantiate (Resources.Load("Prefabs/QuadPlane"));
 			quadPlane[row, col].GetComponent<QuadPlaneScript> ().Init (row, col);
 		}
+	}
+
+	public void teleport(int row, int col) {
+		destroyWorld ();
+		instantiateQuadPlane (row, col);
+	}
+
+	void destroyWorld() {
+		for (int a = 0; a < maxTileRow; a++) {
+			for (int b = 0; b < maxTileCol; b++) {
+				Destroy (quadPlane [a, b]);
+			}
+		}
+
 	}
 }
