d471f0a79bb47050a6309e2529e919510027e29f
Nathan Thomas <nthomas@systemsinmotion.com>
2016-06-05 12:44:11 -0400

    moved load resources up to plane controller

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class PlaneController : MonoBehaviour {
+	public static string mainTextCall = "https://api.nasa.gov/mars-wmts/catalog/Mars_Viking_MDIM21_ClrMosaic_global_232m/1.0.0//default/default028mm/{TileZoom}/{TileRow}/{TileCol}.png";
+	public static string heightTextCall = "https://api.nasa.gov/mars-wmts/catalog/Mars_MGS_MOLA_DEM_mosaic_global_463m_8/1.0.0//default/default028mm/{TileZoom}/{TileRow}/{TileCol}.png";
+	int zoom = 5;
 
 	GameObject[,] quadPlane;
 
@@ -11,9 +14,10 @@ public class PlaneController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		quadPlane = new GameObject[maxTileRow, maxTileCol];
-		 instantiateQuadPlane (12, 8); // Mount Olympus
+		// instantiateQuadPlane (12, 8); // Mount Olympus
 		// instantiateQuadPlane (22, 44); // Martian Cocaine
 		// instantiateQuadPlane (16, 24); // Canyon
+		teleport(12, 8);
 
 		var MattDamon = Instantiate (Resources.Load ("Prefabs/Character/Matt Damon")) as GameObject;
 		MattDamon.transform.position = new Vector3 (128, 128, 128);
@@ -27,16 +31,16 @@ public class PlaneController : MonoBehaviour {
 //			teleport (22, 44);
 	}
 
-	public void instantiateQuadPlane(int row, int col) {
+	public void instantiateQuadPlane(int row, int col, Texture2D mainTexture, Texture2D heightTexture) {
 		if (quadPlane[row,col] == null) {
 			quadPlane[row, col] = (GameObject) Instantiate (Resources.Load("Prefabs/QuadPlane"));
-			quadPlane[row, col].GetComponent<QuadPlaneScript> ().Init (row, col);
+			quadPlane[row, col].GetComponent<QuadPlaneScript> ().Init (mainTexture, heightTexture);
 		}
 	}
 
 	public void teleport(int row, int col) {
 		destroyWorld ();
-		instantiateQuadPlane (row, col);
+		StartCoroutine(loadResources (row, col));
 	}
 
 	void destroyWorld() {
@@ -47,4 +51,28 @@ public class PlaneController : MonoBehaviour {
 		}
 
 	}
+
+	IEnumerator loadResources(int row, int col) {
+		// Create calls
+		string mCall = (string) mainTextCall.Clone();
+		mCall = mCall.Replace("{TileZoom}", zoom.ToString());
+		mCall = mCall.Replace ("{TileRow}", row.ToString());
+		mCall = mCall.Replace ("{TileCol}", col.ToString());
+		string hCall = (string) heightTextCall.Clone();
+		hCall = hCall.Replace("{TileZoom}", zoom.ToString());
+		hCall = hCall.Replace ("{TileRow}", row.ToString());
+		hCall = hCall.Replace ("{TileCol}", col.ToString());
+
+		Debug.Log ("Hey" + hCall);
+		// Make Calls
+		WWW apiMain = new WWW(mCall);
+		yield return apiMain;
+
+		WWW apiHeight = new WWW (hCall);
+		yield return apiHeight;
+
+		// Create Planes
+		instantiateQuadPlane(row, col, apiMain.texture, apiHeight.texture);
+	}
+
 }
--- Assets/Scripts/QuadPlaneScript.cs
+++ Assets/Scripts/QuadPlaneScript.cs
@@ -3,46 +3,19 @@ using System.Collections;
 
 public class QuadPlaneScript : MonoBehaviour {
 
-	public static string mainTextCall = "https://api.nasa.gov/mars-wmts/catalog/Mars_Viking_MDIM21_ClrMosaic_global_232m/1.0.0//default/default028mm/{TileZoom}/{TileRow}/{TileCol}.png";
-	public static string heightTextCall = "https://api.nasa.gov/mars-wmts/catalog/Mars_MGS_MOLA_DEM_mosaic_global_463m_8/1.0.0//default/default028mm/{TileZoom}/{TileRow}/{TileCol}.png";
 
 	public GameObject[,] plane;
 	int maxX = 2; // Divide planes into sub objects of these sizes
 	int maxY = 2;
-	int zoom = 5;
 
 	void Start () {
+	}
+
+	public void Init(Texture2D mainTexture, Texture2D heightTexture) {
 		plane = new GameObject[maxX, maxY];
+		createPlanes (mainTexture, heightTexture);
 	}
 
-	public void Init(int row, int col) {
-		Debug.Log (row);
-		Debug.Log (col);
-		StartCoroutine(loadResources (row, col));
-	}
-
-	IEnumerator loadResources(int row, int col) {
-		// Create calls
-		string mCall = (string) mainTextCall.Clone();
-		mCall = mCall.Replace("{TileZoom}", zoom.ToString());
-		mCall = mCall.Replace ("{TileRow}", row.ToString());
-		mCall = mCall.Replace ("{TileCol}", col.ToString());
-		string hCall = (string) heightTextCall.Clone();
-		hCall = hCall.Replace("{TileZoom}", zoom.ToString());
-		hCall = hCall.Replace ("{TileRow}", row.ToString());
-		hCall = hCall.Replace ("{TileCol}", col.ToString());
-
-		Debug.Log ("Hey" + hCall);
-		// Make Calls
-		WWW apiMain = new WWW(mCall);
-		yield return apiMain;
-
-		WWW apiHeight = new WWW (hCall);
-		yield return apiHeight;
-
-		// Create Planes
-		createPlanes (apiMain.texture, apiHeight.texture);
-	}
 
 	void createPlanes(Texture2D mainTexture, Texture2D heightTexture) {
 		int chunkX = (heightTexture.width / maxX); // Size of a plane chunk
@@ -51,8 +24,6 @@ public class QuadPlaneScript : MonoBehaviour {
 		int startY = 0;
 		int endX = maxX;
 		int endY = maxY;
-//		int endX = 1;
-//		int endY = 1;
 
 		for (int x = startX; x < endX; x++) {
 			for (int y = startX; y < endY; y++) {
