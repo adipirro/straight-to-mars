3de4d0e9e3f8f3471d61ed1b93f13eee2174665d
Jeff Reichel <jreichel@nexient.com>
2016-06-04 21:21:26 -0400

    Fix double click handler.

--- Assets/Scripts/DoubleClick.cs
+++ Assets/Scripts/DoubleClick.cs
@@ -10,14 +10,12 @@ public class DoubleClick : MonoBehaviour {
 
 	void Update() {
 		if (click && Time.time > (clickTime + clickDelta)) {
-			transform.Rotate(new Vector3(0, 180, 0));  // Single click
 			click = false;
 		}
 	}
 
 	void OnMouseDown() {
 		if (click && Time.time <= (clickTime + clickDelta)) {
-			transform.Rotate (new Vector3(180,0,0)); // Double click
 			click = false;
 		}
 		else {
